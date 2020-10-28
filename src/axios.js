6f41230b805a4df4ec516914a27b1d9f16f9e2c2
Mor NDIAYE <ndiaye_c@etna-alternance.net>
2020-10-28 21:07:01 +0100

    [deployment]: prepare to deployment

--- src/axios.js
+++ src/axios.js
@@ -1,15 +1,28 @@
 import axios from 'axios';
 
+let uri = '';
+if (process.env.NODE_ENV === 'production') {
+	uri = 'https://cenator-api.herokuapp.com/';
+} else {
+	uri = process.env.REACT_APP_REST_ENDPOINT;
+}
+
 const axiosInstance = axios.create({
-	baseURL: process.env.REACT_APP_REST_ENDPOINT
+	baseURL: uri
 });
 
-axiosInstance.interceptors.request.use((request) => {
-	return request;
-}, error =>  Promise.reject(error));
+axiosInstance.interceptors.request.use(
+	(request) => {
+		return request;
+	},
+	(error) => Promise.reject(error)
+);
 
-axiosInstance.interceptors.response.use((response) => {
-    return response;
-},  error =>  Promise.reject(error) );
+axiosInstance.interceptors.response.use(
+	(response) => {
+		return response;
+	},
+	(error) => Promise.reject(error)
+);
 
 export default axiosInstance;
