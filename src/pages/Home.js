fcfd58161635c26e5cbe1b9bbf0635c11a5fa0d6
Anagaret <alexis.nagarettiname@gmail.com>
2020-10-29 17:20:51 +0100

    Improvement Pomodoro
    Add a link home to Pomodoro
    Corrected Wikipedia citation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,33 +4,36 @@ import logo from '../assets/logo.png';
 
 const Home = () => {
     const { state } = useContext(AuthContext);
-	return (
+    return (
     <div>
         <div className="text-center">
-          <a href="./teams" className="btn btn-raised btn-secondary" style={{ width:"50%" }}>
+          <a href="./teams" className="btn btn-raised btn-secondary" style={{ width:"34%" }}>
             Voir les équipes
           </a>
-          <a href="./projects" className="btn btn-raised btn-secondary" style={{ width:"50%" }}>Voir les projets</a>
+          <a href="./projects" className="btn btn-raised btn-secondary" style={{ width:"33%" }}>Voir les projets</a>
+            <a href="./pomodoro" className="btn btn-raised btn-danger" style={{ width:"33%" }}>
+              Pomodoro
+            </a>
         </div>
-  		<div className="container pt-5">
+          <div className="container pt-5">
              <div className="jumbotron text-center h1">
                   Welcome to Cenator Timer Manager
              </div>
              <hr/>
               {/* {JSON.stringify(state.user)} */}
-  		</div>
+          </div>
       <div className="container pt-5">
          <div className="text-center h1">
               La création du logo
               <img src={logo} alt="Cenator Timer logo" style={{ height: "20rem" }}/>
          </div>
          <div className="text-center h4">
-            <p> aaaaaaaaaaaaaaaa </p>
+            <p> Le superbe logo : ) </p>
          </div>
          <hr/>
-  		</div>
+          </div>
     </div>
-	);
+    );
 };
 
 export default Home;
--- src/pages/Pomodoro.js
+++ src/pages/Pomodoro.js
@@ -4,29 +4,30 @@ import pomodoro from '../assets/pomodoro.png';
 
 const Home = () => {
     const { state } = useContext(AuthContext);
-	return (
+    return (
     <div>
+      <a href="./teams" className="btn btn-raised btn-secondary" style={{ width:"50%" }}>
+        Voir les équipes
+      </a>
+      <a href="./projects" className="btn btn-raised btn-secondary" style={{ width:"50%" }}>Voir les projets</a>
       <div className="container pt-5">
-        <div className="text-center h4">
-            <p>"La technique Pomodoro est une technique de gestion du temps développée par Francesco Cirillo à la fin des années 1980. 
-                Cette méthode se base sur l'usage d'un minuteur permettant de respecter des périodes de 25 minutes appelées pomodori (qui signifie en italien « tomates »). 
-                Ces différentes périodes de travail sont séparées par de courtes pauses. Proches des concepts de cycles itératifs et des méthodes de développement agiles, 
-                utilisées dans le développement de logiciel, la méthode est utilisée pour la programmation en binôme. 
-                La méthode a pour principale prétention que des pauses régulières favorisent l'agilité intellectuelle. 
+        <div className="text-justify">
+            <p>"La technique Pomodoro est une technique de gestion du temps développée par Francesco Cirillo à la fin des années 1980.
+                Cette méthode se base sur l'usage d'un minuteur permettant de respecter des périodes de 25 minutes appelées pomodori (qui signifie en italien « tomates »).
+                Ces différentes périodes de travail sont séparées par de courtes pauses. Proches des concepts de cycles itératifs et des méthodes de développement agiles,
+                utilisées dans le développement de logiciel, la méthode est utilisée pour la programmation en binôme.
+                La méthode a pour principale prétention que des pauses régulières favorisent l'agilité intellectuelle.
                 Certains bénéfices des temps de repos sur la consolidation de la mémoire peuvent être observés expérimentalement."</p>
-            <p><a href="https://fr.wikipedia.org/wiki/Technique_Pomodoro">-Wikipédia</a></p>
+              <p className="text-center">Source : <a href="https://fr.wikipedia.org/wiki/Technique_Pomodoro">Wikipédia</a></p>
          </div>
-         <div className="text-center h1">
-              Comment ça marche ?
+       <hr/>
+         <div className="text-center">
+              <h3>Comment ça marche ?</h3>
               <img src={pomodoro} alt="pomodoro user guide" style={{ height: "20rem" }}/>
          </div>
-         <hr/>
-  		</div>
+          </div>
     </div>
-	);
+    );
 };
 
 export default Home;
-
-
-
